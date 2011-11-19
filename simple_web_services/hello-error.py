f945f5c1bbfd8720bfff3b61b3e6ef0b77f1f504
Adam Parrish <adam@decontextualize.com>
2011-11-19 15:59:01 -0500

    example for error responses

---
+++ simple_web_services/hello-error.py
@@ -0,0 +1,21 @@
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+define("port", default=8888, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
+	def get(self):
+		greeting = self.get_argument('greeting', 'Hello')
+		self.write(greeting + ', friendly user!')
+	def write_error(self, status_code, **kwargs):
+		self.write("Gosh darnit, user! You caused a %d error." % status_code)
+
+if __name__ == "__main__":
+	tornado.options.parse_command_line()
+	app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+	http_server = tornado.httpserver.HTTPServer(app)
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()

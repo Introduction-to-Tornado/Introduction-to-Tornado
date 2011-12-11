661a171afb48192609e798cb2fa70e0df38d2b78
Adam Parrish <adam@decontextualize.com>
2011-12-11 15:32:21 -0500

    synchronous, asynchronous, asynchronous + tornado.gen examples for http requests

---
+++ async_http/tweet_rate_async.py
@@ -0,0 +1,47 @@
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.httpclient
+
+import urllib
+import json
+import datetime
+import time
+
+from tornado.options import define, options
+define("port", default=8888, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
+	@tornado.web.asynchronous
+	def get(self):
+		query = self.get_argument('q')
+		client = tornado.httpclient.AsyncHTTPClient()
+		client.fetch("http://search.twitter.com/search.json?" + \
+				urllib.urlencode({"q": query, "result_type": "recent", "rpp": 100}),
+				callback=self.on_response)
+
+	def on_response(self, response):
+		body = json.loads(response.body)
+		result_count = len(body['results'])
+		now = datetime.datetime.utcnow()
+		raw_oldest_tweet_at = body['results'][-1]['created_at']
+		oldest_tweet_at = datetime.datetime.strptime(raw_oldest_tweet_at,
+				"%a, %d %b %Y %H:%M:%S +0000")
+		seconds_diff = time.mktime(now.timetuple()) - \
+				time.mktime(oldest_tweet_at.timetuple())
+		tweets_per_second = float(result_count) / seconds_diff
+		self.write("""
+<div style="text-align: center">
+	<div style="font-size: 72px">%s</div>
+	<div style="font-size: 144px">%.02f</div>
+	<div style="font-size: 24px">tweets per second</div>
+</div>""" % (self.get_argument('q'), tweets_per_second))
+		self.finish()
+
+if __name__ == "__main__":
+	tornado.options.parse_command_line()
+	app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+	http_server = tornado.httpserver.HTTPServer(app)
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()

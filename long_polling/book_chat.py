456549f6dce0863e9dc2fb759c9085f40017c43a
Brendan <brendan@plusminusfive.com>
2011-12-14 22:44:39 -0500

    Added a book-related long polling example

--- long_polling/book_chat.py
+++ long_polling/book_chat.py
@@ -1,52 +1,87 @@
 import tornado.web
 import tornado.httpserver
 import tornado.ioloop
+import tornado.options
+from uuid import uuid4
 
-class MessageCenter(object):
+class ShoppingCart(object):
+	inventoryCount = 10
 	callbacks = []
+	carts = {}
 	
 	def register(self, callback):
 		self.callbacks.append(callback)
 	
-	def send(self, message):
+	def moveItemToCart(self, session):
+		if session in self.carts:
+			return
+		
+		self.carts[session] = True
+		self.notifyCallbacks()
+	
+	def removeItemFromCart(self, session):
+		if session not in self.carts:
+			return
+		
+		del(self.carts[session])
+		self.notifyCallbacks()
+	
+	def notifyCallbacks(self):
 		for c in self.callbacks:
-			c(message)
+			c(self.inventoryCount - len(self.carts))
 		self.callbacks = []
-	
-class ChatHandler(tornado.web.RequestHandler):
+
+class DetailHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.write("<html><head>BookChat</head><body><ul><li>chat message</li></ul></body></html>")
-	
-class MessageHandler(tornado.web.RequestHandler):
+		session = uuid4()
+		count = self.application.shoppingCart.inventoryCount
+		self.render("index.html", session=session, count=count)
+
+class CartHandler(tornado.web.RequestHandler):
 	def post(self):
-		self.application.messageCenter.send(self.get_argument('m'))
-		self.set_status(200)
-		self.write('{"success":true}')
-	
+		action = self.get_argument('action')
+		session = self.get_argument('session')
+		
+		if not session:
+			self.set_status(400)
+			return
+		
+		if action == 'add':
+			self.application.shoppingCart.moveItemToCart(session)
+		elif action == 'remove':
+			self.application.shoppingCart.removeItemFromCart(session)
+		else:
+			self.set_status(400)
+
+class StatusHandler(tornado.web.RequestHandler):
 	@tornado.web.asynchronous
 	def get(self):
-		self.application.messageCenter.register(self.async_callback(self.on_message))
+		self.application.shoppingCart.register(self.async_callback(self.on_message))
 	
-	def on_message(self, message):
-		self.write('{"message":"%s"}' % message)
+	def on_message(self, count):
+		self.write('{"inventoryCount":"%d"}' % count)
 		self.finish()
 		
 class Application(tornado.web.Application):
 	def __init__(self):
-		self.messageCenter = MessageCenter()
+		self.shoppingCart = ShoppingCart()
 		
 		handlers = [
-			(r'/chat', ChatHandler),
-			(r'/messages', MessageHandler)
+			(r'/', DetailHandler),
+			(r'/cart', CartHandler),
+			(r'/cart/status', StatusHandler)
 		]
 		
 		settings = {
-			'template_path': 'templates'
+			'template_path': 'templates',
+			'static_path': 'static'
 		}
 		
 		tornado.web.Application.__init__(self, handlers, **settings)
 
 if __name__ == '__main__':
+	tornado.options.parse_command_line()
+	
 	app = Application()
 	server = tornado.httpserver.HTTPServer(app)
 	server.listen(8000)

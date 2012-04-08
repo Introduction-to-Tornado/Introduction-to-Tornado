c7350c6b986b058fac9d4e2640c55052a467bb4a
Mike Dory <lists@doryexmachina.com>
2012-04-08 12:36:02 -0300

    Switching to port 8000 for consistency.

--- databases/definitions_readwrite.py
+++ databases/definitions_readwrite.py
@@ -6,7 +6,7 @@ import tornado.web
 import pymongo
 
 from tornado.options import define, options
-define("port", default=8888, help="run on the given port", type=int)
+define("port", default=8000, help="run on the given port", type=int)
 
 class Application(tornado.web.Application):
 	def __init__(self):
